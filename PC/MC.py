bdf4f88ae5748daf8e3e92ce23c41b7bee55cba6
Kristian.Jensen <Kristian.Jensen@student.uib.no>
2022-05-19 14:49:30 +0200

    test runs

--- PC/MC.py
+++ PC/MC.py
@@ -20,7 +20,7 @@ import HelperFunctions as HF
 import PC_CRC
 import test_CRC
 
-run_vals = [[2/5, 0.14, 15, 'BSC'], [2/5, 0.12, 15, 'BSC'], [2/5, 0.1, 15, 'BSC'], [2/5, 0.08, 15, 'BSC']]
+run_vals = [[1/3, 0.17, 15, 'BSC'], [1/3, 0.15, 15, 'BSC'], [1/3, 0.13, 15, 'BSC'],[1/3, 0.1, 15, 'BSC']]
 I_IL = 0
 runs = 5000
 decoder = 'SC'
@@ -51,7 +51,6 @@ for elem in run_vals:
         N0 = 2 * sigma ** 2
 
     BLER, BER = 0, 0
-    print(elem)
     start_time = time.time()
     print(elem)
     for iteration in range(runs):
@@ -62,7 +61,12 @@ for elem in run_vals:
             print(iteration)
             a = random_vector(GF(2), A)
             c, G = test_CRC.CRC(a, A, pol)
+            #print(f'c:\n{c}')
             c_ap, PI = PC_Input_Bits_Interleaver.interleaver(I_IL=I_IL, c_seq=c)
+            #print(f'c_ap:\n{c_ap}')
+            testi = test_CRC.CRC_check(c, K, pol)
+            #testt = test_CRC.ICRC_check(c_ap, len(c_ap), [17,28,33,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52,53,54,55], K, PI)
+            #breakpoint()
             #print(c_ap)
             u = PC_Subchannel_Allocation.calc_u(N, QNI, c_ap, QNPC)
             d = vector(GF(2), u) * GN
--- PC/test_CRC.py
+++ PC/test_CRC.py
@@ -55,19 +55,39 @@ def CRC_check(a, A, pol):
     return res
 
 
-def ICRC_check(a, A, iPI):
+def ICRC_check(a, A, iPI, K, PI):
+    crcbits= []
+    for elem in iPI[::-1]:
+        crcbits.insert(0, a[elem])
+
+    infbits = [i for i in a]
+    for a in iPI[::-1]:
+        print(a)
+        infbits.pop(a)
+
+    cword = vector(GF(2), list(infbits) + list(crcbits))
     breakpoint()
-    map(lambda x: a.append(a.pop(x)), iPI)
+    #cword = list(map(lambda x: a.append(a.pop(x)), iPI))
+    #cword = vector(GF(2), list(a:[]))
     pol = x**24 + x**23 + x**21 + x**20 + x**17 + x**15 + x**13 + x**12 + x**8 + x**4 + x**2 + x + x**0
     P = pol.degree()
-    C = zero_matrix(GF(2), A, P)
+    C = zero_matrix(GF(2), K, P)
     C[-1] = vector(GF(2), pol.list()[::-1][1:])
-    k = A-2
+    k = K-2
     while k >= 0:
         for i in range(P-1):
             C[k, i] = C[k+1, i+1] + C[k+1, 0] * pol[P - (i+1)]
             C[k, P-1] = C[k+1, 0] * pol[0]
         k = k-1
-    res = vector(Matrix(GF(2), a)*C).list()
+    cc = Matrix(GF(2), [C[a] for a in PI])
+    breakpoint()
+    testi = cword*cc
+    Cap = cc[:A].rows()
+    #Cap.append(vector(GF(2), [a for a in cc[iPI[0]]]))
+    Cap = Matrix(GF(2), Cap)
+
+    res = vector(GF(2), a) *Cap
+    print(f'res:{res}')
+    breakpoint()
     return res
 
